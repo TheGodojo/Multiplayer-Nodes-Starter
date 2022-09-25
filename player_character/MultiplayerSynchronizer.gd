c8ae5f074a34218f14257c1326b21f91e95da0e5
TheGodojo <104181056+TheGodojo@users.noreply.github.com>
2022-09-25 19:55:35 -0400

    Add files via upload

---
+++ player_character/MultiplayerSynchronizer.gd
@@ -0,0 +1,16 @@
+extends MultiplayerSynchronizer
+
+@export var position:Vector3:
+	set(val):
+		if is_multiplayer_authority():
+			position = val
+		else:
+			get_parent().position = val
+			
+			
+@export var y_rotation:float:
+	set(val):
+		if is_multiplayer_authority():
+			y_rotation = val
+		else:
+			get_parent().rotation.y = val
